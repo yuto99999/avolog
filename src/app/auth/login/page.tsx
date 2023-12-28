dfdf668f4f5842e09c44324a3486d0971eefdc35
yuto99999 <myuto2003@gmail.com>
2023-12-28 16:45:24 +0900

    auth関連のページ3つ作成

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/app/components/Auth/Login/login";
+
+export default function Home() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import Register from "@/app/components/Auth/Register/register";
+
+export default function Home() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
