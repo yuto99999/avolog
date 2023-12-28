69c57e154721b2dd860aa5ac8468e1cb3c7c28a2
yuto99999 <myuto2003@gmail.com>
2023-12-28 16:43:57 +0900

    ファイル削除

--- src/app/_components/organisms/main.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@mui/material";
-
-export default function Main({ children }: { children: React.ReactNode }) {
-  return (
-    <Container maxWidth={false} disableGutters>
-      {children}
-    </Container>
-  );
-}
