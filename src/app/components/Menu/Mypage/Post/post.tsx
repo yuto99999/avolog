62900427fb799a0627dc2f3781d4913dc21dad76
yuto99999 <myuto2003@gmail.com>
2023-12-30 01:19:02 +0900

    SaveとPost新規作成(タブパネルで使用する)

---
+++ src/app/components/Menu/Mypage/Post/post.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Post = () => {
+  return (
+    <Box bgcolor="#424242" width="100%" height="100vh">
+      <Typography>Post</Typography>
+    </Box>
+  );
+};
+
+export default Post;
---
+++ src/app/components/Menu/Mypage/Save/save.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Save = () => {
+  return (
+    <Box bgcolor="#00bfff" width="100%" height="100vh">
+      <Typography>Save</Typography>
+    </Box>
+  );
+};
+
+export default Save;
