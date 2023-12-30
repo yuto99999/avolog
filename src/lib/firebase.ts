09c4e7f27b0dd8478484c97af88b0b562d3b1119
yuto99999 <myuto2003@gmail.com>
2023-12-30 22:59:56 +0900

    firebaseConfig設定

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+};
+
+if (!getApps()?.length) {
+  initializeApp(firebaseConfig);
+}
+
+export const auth = getAuth();
+export const db = getFirestore();
