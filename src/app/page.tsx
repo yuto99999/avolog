63254f89e5d827522b90a689b78d868ec0b1f6da
yuto99999 <myuto2003@gmail.com>
2023-12-31 14:02:33 +0900

    page.tsx(app)に<Header />と<Footer />追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import Top from "./components/Top/top";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <Top />
+      <Footer />
     </>
   );
 }
