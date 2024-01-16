a26761d8df1be21789a4533bf1f0ec6bc4fb4ef5
yuto99999 <myuto2003@gmail.com>
2024-01-16 22:49:31 +0900

    <DisplayShop />追加とsx追加

--- src/app/components/Menu/Shop/shop.tsx
+++ src/app/components/Menu/Shop/shop.tsx
@@ -1,11 +1,18 @@
 "use client";
 import { Box, Typography } from "@mui/material";
 import Post from "./Post/post";
+import DisplayShop from "./displayShop";
 
 const Shop = () => {
   return (
-    <Box width="100%" minHeight="100vh" mt="10rem" >
-      <Post />
+    <Box
+      width="100%"
+      minHeight="100vh"
+      pt="6.5rem"
+      sx={{ backgroundColor: "#F8F9F9" }}
+    >
+      {/* <Post /> */}
+      <DisplayShop />
     </Box>
   );
 };
