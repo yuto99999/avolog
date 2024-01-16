dd17d3cb13c15d205a97ac393ff77bf5213cab74
yuto99999 <myuto2003@gmail.com>
2024-01-17 01:19:10 +0900

    投稿内容の追加表示とGridを使って表示

--- src/app/components/Menu/Shop/displayShop.tsx
+++ src/app/components/Menu/Shop/displayShop.tsx
@@ -1,6 +1,8 @@
-import { Box, Typography, Avatar, Rating, styled } from "@mui/material";
 import { Timestamp } from "firebase/firestore";
 import useFirebase from "@/lib/useFirebase";
+import { Box, Typography, Avatar, Rating, styled, Grid } from "@mui/material";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 interface Shop {
   id: string;
@@ -22,30 +24,52 @@ const DisplayShop = () => {
   const { documents: shops } = useFirebase("Shop");
 
   return (
-    <Box p={3}>
-      {shops.map((shop: Shop) => (
-        <Box
-          key={shop.id}
-          width="30%"
-          sx={{ backgroundColor: "#ffffff", borderRadius: 5 }}
-        >
-          <Box display="flex" alignItems="center" p={1}>
-            <Avatar src={shop.user.image ? shop.user.image : ""} alt="" />
-            <Typography pl={1.5}>{shop.user.name}</Typography>
-          </Box>
-          <StyledImage src={"../img/top/top2.png"} />
-          <Typography p={0.7}>{shop.name}</Typography>
-          <Typography p={0.7}>{shop.address}</Typography>
-          <Typography p={0.7}>{shop.budget}</Typography>
-          <Rating
-            name="half-rating-read"
-            defaultValue={shop.rate}
-            precision={0.5}
-            readOnly
-            sx={{ p: 1 }}
-          />
-        </Box>
-      ))}
+    <Box p={3} pl={5} display="flex">
+      <Grid container spacing={2}>
+        {shops.map((shop: Shop) => (
+          <Grid
+            item
+            xs={12}
+            md={4}
+            key={shop.id}
+            display="flex"
+            justifyContent="center"
+          >
+            <Box
+              width="85%"
+              mb={5}
+              sx={{ backgroundColor: "#ffffff", borderRadius: 3 }}
+            >
+              <Box display="flex" alignItems="center" p={1}>
+                <Avatar src={shop.user.image ? shop.user.image : ""} alt="" />
+                <Typography pl={1.5}>{shop.user.name}</Typography>
+              </Box>
+              <StyledImage src={"../img/top/top2.png"} />
+              <Typography p={0.7}>{shop.name}</Typography>
+              <Typography p={0.7}>{shop.address}</Typography>
+              <Box display="flex">
+                <Box p={0.7} display="flex" alignItems="center">
+                  <LightModeIcon fontSize="small" />
+                  <Typography pl={0.5}>{shop.budget}</Typography>
+                </Box>
+                <Box p={0.7} display="flex" alignItems="center">
+                  <BedtimeIcon fontSize="small" />
+                  <Typography pl={0.5}>{shop.budget}</Typography>
+                </Box>
+              </Box>
+              <Box p={0.7} pb={1.3} display="flex" alignItems="center">
+                <Rating
+                  name="half-rating-read"
+                  defaultValue={shop.rate}
+                  precision={0.1}
+                  readOnly
+                />
+                <Typography pl={1}>{shop.rate}</Typography>
+              </Box>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
