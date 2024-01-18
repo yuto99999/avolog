a1c921adc21c84446cd9018a5a27c7a765f006c8
yuto99999 <myuto2003@gmail.com>
2024-01-18 23:54:37 +0900

    お店クチコミランキングTop５表示

--- src/app/components/Top/shopRank.tsx
+++ src/app/components/Top/shopRank.tsx
@@ -1,15 +1,28 @@
+import useRate from "@/lib/useRate";
 import {
   Box,
   Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
   Typography,
   styled,
   Link,
+  Grid,
+  Rating,
 } from "@mui/material";
 
+interface Shop {
+  id: string;
+  category: string;
+  name: string;
+  prefecture: string;
+  rate: number;
+  user: {
+    uid: string;
+  };
+}
+
 const ShopRank = () => {
+  const { documents: shops } = useRate("Shop");
+
   return (
     <Box width="100%" mb={2}>
       <Box display="flex" justifyContent="space-between" p={15} pt={5} pb={2}>
@@ -18,34 +31,65 @@ const ShopRank = () => {
         </Typography>
         <StyledLink href={"/menu/shop"}>お店をもっと見る</StyledLink>
       </Box>
-      <Box pr={15} pl={15}>
-        <Card sx={{ width: "14rem" }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="130"
-              image="../img/top/top1.png"
-            />
-            <CardContent>
-              <Typography
-                component="div"
-                fontFamily="游ゴシック"
-                fontSize="1.3rem"
-                fontWeight="bold"
+      <Box pr={15} pl={15} display="flex">
+        <Grid container spacing={2.5}>
+          {shops.map((shop: Shop) => (
+            <Grid
+              item
+              xs={12}
+              md={2.4}
+              key={shop.id}
+              display="flex"
+              justifyContent="center"
+            >
+              <Card
+                sx={{
+                  width: "100%",
+                  mb: 5,
+                  boxShadow: 3,
+                  borderRadius: 3,
+                }}
               >
-                アボカド食堂
-              </Typography>
-              <Typography
-                color="text.secondary"
-                fontFamily="游ゴシック"
-                fontSize=".9rem"
-                fontWeight="bold"
-              >
-                東京都千代田区
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+                <StyledImage src={"../img/top/top2.png"} />
+                <Typography
+                  pt={1.5}
+                  pl={1.8}
+                  fontFamily="游ゴシック"
+                  fontWeight="bold"
+                  fontSize="1rem"
+                >
+                  {shop.name}
+                </Typography>
+                <Typography
+                  pt={1.2}
+                  pl={1.8}
+                  fontFamily="游ゴシック"
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  color="GrayText"
+                >
+                  {shop.prefecture}
+                </Typography>
+                <Box display="flex" alignItems="center" p={1.2}>
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={shop.rate}
+                    precision={0.1}
+                    readOnly
+                  />
+                  <Typography
+                    pl={1}
+                    fontFamily="游ゴシック"
+                    fontWeight="bold"
+                    fontSize="0.9rem"
+                  >
+                    {shop.rate}
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Box>
   );
@@ -57,6 +101,13 @@ const StyledLink = styled(Link)(({ theme }) => ({
   fontWeight: 500,
   fontSize: "1.2rem",
   marginLeft: 2,
+}));
+
+const StyledImage = styled("img")(({ theme }) => ({
+  display: "block",
+  width: "100%",
+  height: "9rem",
+  objectFit: "cover",
 }));
 
 export default ShopRank;
