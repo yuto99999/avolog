75282060470eb4334ecebea547be68adacbd8b35
yuto99999 <myuto2003@gmail.com>
2024-01-19 15:25:20 +0900

    ジャンルのデータリスト作成

---
+++ src/data/Genre.ts
@@ -0,0 +1,30 @@
+export type Genre = {
+  code: number;
+  name: string;
+};
+export const GenreList: Genre[] = [
+  {
+    code: 1,
+    name: "和食",
+  },
+  {
+    code: 2,
+    name: "メキシカン",
+  },
+  {
+    code: 3,
+    name: "洋食",
+  },
+  {
+    code: 4,
+    name: "中華",
+  },
+  {
+    code: 5,
+    name: "アジア・エスニック",
+  },
+  {
+    code: 6,
+    name: "その他",
+  },
+];
