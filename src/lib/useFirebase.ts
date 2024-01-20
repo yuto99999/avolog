72884b8e11a0c4abc59e13d1f421d282d5e58319
yuto99999 <myuto2003@gmail.com>
2024-01-20 23:58:32 +0900

    条件、desc or asc を決めれるように変更

--- src/lib/useFirebase.ts
+++ src/lib/useFirebase.ts
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import { store } from "@/lib/firebase";
 
-const useFirebase = (data: string) => {
+const useFirebase = (data: string, condition: string, type: "asc" | "desc") => {
   const [documents, setDocuments] = useState([]);
 
   useEffect(() => {
     const firestore = store;
     const docRef = collection(firestore, data);
-    const queryRef = query(docRef, orderBy("createdAt", "desc"));
+    const queryRef = query(docRef, orderBy(condition, type));
     const unsub = onSnapshot(queryRef, (snapshot) => {
       let results: any = [];
       snapshot.docs.forEach((doc) => {
