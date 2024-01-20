0abb154be63c5ba3ec30301b2b2a476aa11f2fce
yuto99999 <myuto2003@gmail.com>
2024-01-20 13:20:45 +0900

    pt変更

--- src/app/components/Menu/Shop/shop.tsx
+++ src/app/components/Menu/Shop/shop.tsx
@@ -5,7 +5,7 @@ import SerchBox from "./serchBox";
 
 const Shop = () => {
   return (
-    <Box width="100%" minHeight="100vh" pt="6.5rem">
+    <Box width="100%" minHeight="100vh" pt="8rem">
       <SerchBox />
       <DisplayShop />
     </Box>
