ccc66cac84a0633b25f76feaccbf6dc5e6af5c66
yuto99999 <myuto2003@gmail.com>
2024-01-20 13:20:02 +0900

    投稿ボタンのデザイン変更

--- src/app/components/Menu/Shop/serchBox.tsx
+++ src/app/components/Menu/Shop/serchBox.tsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Card, Typography, styled } from "@mui/material";
+import CreateIcon from "@mui/icons-material/Create";
 
 const SerchBox = () => {
   const router = useRouter();
@@ -10,17 +11,32 @@ const SerchBox = () => {
   };
 
   return (
-    <Box
-      width="100%"
-      height="5rem"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-evenly"
-    >
-      <Typography>ジャンル</Typography>
-      <Button onClick={onClick}>投稿</Button>
+    <Box pl={12} pr={12} mb={3}>
+      <Card
+        sx={{
+          width: "100%",
+          height: "8rem",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          boxShadow: 3,
+          borderRadius: 3,
+        }}
+      >
+        <Typography>ジャンル</Typography>
+        <StyledBtn onClick={onClick} variant="contained">
+          <CreateIcon />
+          投稿する
+        </StyledBtn>
+      </Card>
     </Box>
   );
 };
+
+const StyledBtn = styled(Button)(({ theme }) => ({
+  fontFamily: "游ゴシック",
+  fontWeight: "bold",
+  fontSize: "1rem",
+}));
 
 export default SerchBox;
