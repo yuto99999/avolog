d409481a2ba08096536214649c2cb7e12a59f412
yuto99999 <myuto2003@gmail.com>
2024-01-21 14:59:00 +0900

    import文整理

--- src/lib/useProfile.ts
+++ src/lib/useProfile.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Auth, getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import {
   collection,
   query,
