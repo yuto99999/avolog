14023c2acbf87bace5d2ae633d7740cb49f80d90
yuto99999 <myuto2003@gmail.com>
2024-01-22 19:58:20 +0900

    削除の際、popover表示

--- src/app/components/Menu/Mypage/delateBtn.tsx
+++ src/app/components/Menu/Mypage/delateBtn.tsx
@@ -1,23 +1,65 @@
+import React from "react";
 import { store } from "@/lib/firebase";
 import { deleteDoc, doc } from "firebase/firestore";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Popover, Typography } from "@mui/material";
 
-const DeleteBtn = ({ id }: { id: string }) => {
-  const handleClick = async () => {
+const BasicPopover = ({ docId }: { docId: string }) => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  const doDelete = async () => {
     const firestore = store;
 
-    const docRef = doc(firestore, "Shop", id);
+    const docRef = doc(firestore, "Shop", docId);
 
     await deleteDoc(docRef);
   };
 
   return (
     <Box>
-      <Button onClick={handleClick} sx={{ color: "#000000" }}>
+      <Button aria-describedby={id} color="error" onClick={handleClick}>
         削除
       </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Box width="22rem" p={2}>
+          <Typography p={1} fontFamily="游ゴシック" >この投稿を削除しますか？</Typography>
+          <Box  display="flex" justifyContent="right" gap={1} p={1}>
+            <Button onClick={doDelete} variant="contained">
+              はい
+            </Button>
+            <Button onClick={handleClose} variant="outlined">
+              いいえ
+            </Button>
+          </Box>
+        </Box>
+      </Popover>
     </Box>
   );
 };
 
-export default DeleteBtn;
+export default BasicPopover;
