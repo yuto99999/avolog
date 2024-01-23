6b260a9783b3eb62503fccf95f1efbfed054fc8c
yuto99999 <myuto2003@gmail.com>
2024-01-23 16:44:52 +0900

    全体的に文字を小さくし、fontWeight:"bold"を解除 / デザイン変更

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,50 +4,58 @@ import AccountMenu from "./accountMenu";
 
 const Header = () => {
   return (
-    <StyledHeader>
-      <Box display="flex" alignItems="center" gap={10} ml={12}>
-        <StyledLink href={"/"} display="flex" alignItems="center">
-          <StyledAvatar alt="AvoLogロゴ" src="../img/AvoLogo.png" />
-          <StyledTitle>AvoLog</StyledTitle>
-        </StyledLink>
-        <StyledLink href={"/menu/shop"}>お店</StyledLink>
-        <StyledLink href={"/menu/recipe"}>レシピ</StyledLink>
-        <StyledLink href={"/menu/mypage"}>マイページ</StyledLink>
-      </Box>
-      <Box display="flex" alignItems="center" gap={3} mr={10}>
-        <StyledLink href={"/auth/register"}>会員登録</StyledLink>
-        <StyledLink href={"/auth/login"}>ログイン</StyledLink>
-        <AccountMenu />
-      </Box>
-    </StyledHeader>
+    <HeaderContainer>
+      <HeaderContents>
+        <Box display="flex" alignItems="center" gap={5} ml={7}>
+          <StyledLink href={"/"}>
+            <Title>AvoLog</Title>
+          </StyledLink>
+          <StyledLink href={"/menu/shop"}>お店</StyledLink>
+          <StyledLink href={"/menu/recipe"}>レシピ</StyledLink>
+          <StyledLink href={"/menu/mypage"}>マイページ</StyledLink>
+        </Box>
+        <Box display="flex" alignItems="center" gap={3} mr={5}>
+          <StyledLink href={"/auth/register"}>サイトについて</StyledLink>
+          <StyledLink href={"/auth/register"}>会員登録</StyledLink>
+          <StyledLink href={"/auth/login"}>ログイン</StyledLink>
+          <AccountMenu />
+        </Box>
+      </HeaderContents>
+    </HeaderContainer>
   );
 };
 
-const StyledHeader = styled(Box)(({ theme }) => ({
+const HeaderContainer = styled(Box)(({ theme }) => ({
   width: "100%",
-  height: "13vh",
-  backgroundColor: "#F1F4E1",
+  height: "15vh",
+  backgroundColor: "#ffffff",
   display: "flex",
   alignItems: "center",
-  justifyContent: "space-between",
+  justifyContent: "center",
   position: "fixed",
   top: 0,
   left: 0,
   zIndex: 999,
 }));
 
-const StyledAvatar = styled(Avatar)(({ theme }) => ({
-  width: 65,
-  height: 65,
-  marginRight: 10,
+const HeaderContents = styled(Box)(({ theme }) => ({
+  width: "95%",
+  height: "10vh",
+  backgroundColor: "#F1F4E1",
+  marginTop: "1.5vh",
+  marginBottom: "1.5vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  borderRadius: "5rem",
 }));
 
-const StyledTitle = styled(Typography)(({ theme }) => ({
+const Title = styled(Typography)(({ theme }) => ({
   textDecoration: "none",
   color: "#000000",
   fontFamily: "游ゴシック",
   fontWeight: "bold",
-  fontSize: "1.5rem",
+  fontSize: "1.3rem",
   letterSpacing: 1,
 }));
 
@@ -55,8 +63,7 @@ const StyledLink = styled(Link)(({ theme }) => ({
   textDecoration: "none",
   color: "#000000",
   fontFamily: "游ゴシック",
-  fontWeight: "bold",
-  fontSize: "1.3rem",
+  fontSize: "1rem",
   letterSpacing: 1,
 }));
 
