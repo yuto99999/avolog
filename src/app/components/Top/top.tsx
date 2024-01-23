c3894c89b1791c10e86fd84fec0887a9d4c16a83
yuto99999 <myuto2003@gmail.com>
2024-01-23 16:43:48 +0900

    mtの変更 / import文整理

--- src/app/components/Top/top.tsx
+++ src/app/components/Top/top.tsx
@@ -1,11 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Slider from "./slider";
 import ShopRank from "./shopRank";
 import RecipeRank from "./recipeRank";
 
 const Top = () => {
   return (
-    <Box width="100%" mt="7rem">
+    <Box width="100%" mt="7.5rem">
       <Slider />
       <ShopRank />
       <RecipeRank />
