996f66bc480a98be1fb7795a0d2665e78a3683a5
yuto99999 <myuto2003@gmail.com>
2024-01-24 16:50:28 +0900

    仮でレシピのランキングにお店のランキング表示

--- src/app/components/Top/recipeRank.tsx
+++ src/app/components/Top/recipeRank.tsx
@@ -1,51 +1,94 @@
+import useRate from "@/lib/useRate";
 import {
   Box,
   Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
   Typography,
   styled,
   Link,
+  Grid,
+  Rating,
 } from "@mui/material";
 
+interface Shop {
+  id: string;
+  image: string;
+  name: string;
+  prefecture: string;
+  rate: number;
+}
+
 const RecipeRank = () => {
+  const { documents: shops } = useRate("Shop");
+
   return (
-    <Box width="100%" mb={5}>
+    <Box width="100%" mb={2}>
       <Box display="flex" justifyContent="space-between" p={15} pt={5} pb={2}>
         <Typography fontFamily="游ゴシック" fontSize="1.5rem" fontWeight="bold">
           人気のレシピ ランキング
         </Typography>
-        <StyledLink href={"/menu/recipe"}>レシピをもっと見る</StyledLink>
+        <StyledLink href={"/menu/shop"}>レシピをもっと見る</StyledLink>
       </Box>
-      <Box pr={15} pl={15}>
-        <Card sx={{ width: "14rem" }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="130"
-              image="../img/top/top3.png"
-            />
-            <CardContent>
-              <Typography
-                component="div"
-                fontFamily="游ゴシック"
-                fontSize="1.3rem"
-                fontWeight="bold"
+      <Box pr={15} pl={15} display="flex">
+        <Grid container spacing={2.5}>
+          {shops.map((shop: Shop) => (
+            <Grid
+              item
+              xs={12}
+              md={2.4}
+              key={shop.id}
+              display="flex"
+              justifyContent="center"
+            >
+              <Card
+                sx={{
+                  width: "100%",
+                  mb: 5,
+                  boxShadow: 3,
+                  borderRadius: 3,
+                }}
               >
-                アボカドサーモン丼
-              </Typography>
-              <Typography
-                color="text.secondary"
-                fontFamily="游ゴシック"
-                fontSize=".9rem"
-                fontWeight="bold"
-              >
-                難易度1
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+                <StyledImage src={shop.image} />
+                <Box height="5.5rem">
+                  <Typography
+                    pt={1.5}
+                    pl={1.8}
+                    fontFamily="游ゴシック"
+                    fontWeight="bold"
+                    fontSize="1rem"
+                  >
+                    {shop.name}
+                  </Typography>
+                  <Typography
+                    pt={1.2}
+                    pl={1.8}
+                    fontFamily="游ゴシック"
+                    fontWeight="bold"
+                    fontSize="0.85rem"
+                    color="GrayText"
+                  >
+                    {shop.prefecture}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="center" p={1.2}>
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={shop.rate}
+                    precision={0.1}
+                    readOnly
+                  />
+                  <Typography
+                    pl={1}
+                    fontFamily="游ゴシック"
+                    fontWeight="bold"
+                    fontSize="0.9rem"
+                  >
+                    {shop.rate}
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Box>
   );
@@ -57,6 +100,13 @@ const StyledLink = styled(Link)(({ theme }) => ({
   fontWeight: 500,
   fontSize: "1.2rem",
   marginLeft: 2,
+}));
+
+const StyledImage = styled("img")(({ theme }) => ({
+  display: "block",
+  width: "100%",
+  height: "9rem",
+  objectFit: "cover",
 }));
 
 export default RecipeRank;
