a043c344b9fb8b99c5944a9f0e1f7aed42499f9b
yuto99999 <myuto2003@gmail.com>
2024-01-30 14:14:15 +0900

    言葉追加

--- src/app/components/About/content.tsx
+++ src/app/components/About/content.tsx
@@ -2,8 +2,16 @@ import { Box, Typography } from "@mui/material";
 
 const Content = () => {
   return (
-    <Box width="100%" mt="7rem" bgcolor="green">
-      <Typography>内容</Typography>
+    <Box width="100%" mt="7rem">
+      <Box width="100%" pl="20%">
+        <Typography fontFamily="游ゴシック" fontSize="2rem">
+          アボラーの、
+          <br />
+          アボラーによる、
+          <br />
+          アボラーのためのサイト
+        </Typography>
+      </Box>
     </Box>
   );
 };
