2986212c6865038014d8725de6a34f76f5b8f91b
yuto99999 <myuto2003@gmail.com>
2024-03-03 00:32:31 +0900

    詳細ボタン作成

---
+++ src/app/components/Menu/Recipe/detailBtn.tsx
@@ -0,0 +1,18 @@
+import { Box, Button } from "@mui/material";
+import Link from "next/link";
+
+const DetailBtn = ({ docId }: { docId: string }) => {
+  return (
+    <Box>
+      <Link href={`/menu/recipe/${docId}`}>
+        <Button
+          variant="text"
+          sx={{ color: "#000000", marginRight: 2, fontSize: "0.9rem" }}
+        >
+          詳しく
+        </Button>
+      </Link>
+    </Box>
+  );
+};
+export default DetailBtn;
